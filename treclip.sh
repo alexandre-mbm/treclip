a0d4eb4ffc0456893270055a42312a2e6358fa33
Alexandre Magno <alexandre.mbm@gmail.com>
2017-05-03 07:36:20 -0300

    48x48

--- treclip.sh
+++ treclip.sh
@@ -2,7 +2,7 @@
 
 yad --on-top --form --undecorated --no-buttons \
     --columns="1" \
-    --field="!/usr/share/icons/hicolor/32x32/apps/firefox.png! Firefox :fbtn" "bash from-firefox.sh" \
-    --field="!/usr/share/icons/hicolor/32x32/apps/google-chrome.png! Chrome :fbtn" "bash from-chrome.sh" &
+    --field="!/usr/share/icons/hicolor/48x48/apps/firefox.png! Firefox :fbtn" "bash from-firefox.sh" \
+    --field="!/usr/share/icons/hicolor/48x48/apps/google-chrome.png! Chrome :fbtn" "bash from-chrome.sh" &
 
 wmctrl -a "Yad"
